2e80fea83ae2df2e0774eb3525d4892461844fe5
qida <sunqida@foxmail.com>
2018-06-23 10:13:30 +0800

    update

--- tcp_server.go
+++ tcp_server.go
@@ -86,26 +86,6 @@ func (s *server) listenChannels() {
 	}
 }
 
-// Start network server
-func (s *server) Listen() {
-	go s.listenChannels()
-
-	listener, err := net.Listen("tcp", s.address)
-	if err != nil {
-		log.Fatal("Error starting TCP server.")
-	}
-	defer listener.Close()
-
-	for {
-		conn, _ := listener.Accept()
-		kaConn, _ := tcpkeepalive.EnableKeepAlive(conn)
-		kaConn.SetKeepAliveIdle(30 * time.Second)
-		kaConn.SetKeepAliveCount(4)
-		kaConn.SetKeepAliveInterval(5 * time.Second)
-		// s.joins <- conn
-		s.joins <- kaConn
-	}
-}
 
 // Creates new tcp server instance
 func New(address string) *server {
